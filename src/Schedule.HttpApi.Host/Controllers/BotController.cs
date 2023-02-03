5f9d2340728d47034381a77284f7461cd53221a2
Sergey Levkovets <sergey.levkovets@soxes.ch>
2023-02-03 20:02:59 +0300

    SCH-3: Bot init

---
+++ src/Schedule.HttpApi.Host/Controllers/BotController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Schedule.Filters;
+using Schedule.Services;
+using Telegram.Bot.Types;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace Schedule.Controllers;
+
+[Route("bot")]
+public class BotController : AbpController
+{
+    [HttpPost]
+    [ValidateTelegramBot]
+    public async Task<IActionResult> Post(
+        [FromBody] Update update,
+        [FromServices] UpdateHandlers handleUpdateService,
+        CancellationToken cancellationToken)
+    {
+        await handleUpdateService.HandleUpdateAsync(update, cancellationToken);
+        return Ok();
+    }
+}
--- src/Schedule.HttpApi.Host/Program.cs
+++ src/Schedule.HttpApi.Host/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Schedule.Controllers;
+using Schedule.Extensions;
+using Schedule.Services;
+using Schedule.Settings;
 using Serilog;
 using Serilog.Events;
+using Telegram.Bot;
 
 namespace Schedule;
 
@@ -28,13 +34,40 @@ public class Program
         try
         {
             Log.Information("Starting Schedule.HttpApi.Host.");
+            
             var builder = WebApplication.CreateBuilder(args);
+            
+            
+            var botConfigurationSection = builder.Configuration.GetSection(BotConfiguration.Configuration);
+            builder.Services.Configure<BotConfiguration>(botConfigurationSection);
+
+            var botConfiguration = botConfigurationSection.Get<BotConfiguration>();
+
+            builder.Services
+                .AddControllers()
+                .AddNewtonsoftJson();
+// Add services to the container.
+
+            builder.Services.AddHttpClient("telegram_bot_client")
+                .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+                {
+                    BotConfiguration? botConfig = sp.GetConfiguration<BotConfiguration>();
+                    TelegramBotClientOptions options = new(botConfig.BotToken);
+                    return new TelegramBotClient(options, httpClient);
+                });
+
+            builder.Services.AddScoped<UpdateHandlers>();
+
+            builder.Services.AddHostedService<ConfigureWebhook>();
+            
             builder.Host
                 .AddAppSettingsSecretsJson()
                 .UseAutofac()
                 .UseSerilog();
             await builder.AddApplicationAsync<ScheduleHttpApiHostModule>();
             var app = builder.Build();
+            
+            //app.MapBotWebhookRoute<BotController>(route: botConfiguration.Route);
             await app.InitializeApplicationAsync();
             await app.RunAsync();
             return 0;
