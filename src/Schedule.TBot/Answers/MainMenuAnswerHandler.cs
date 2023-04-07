f045f830bc18d3a00763e68f75a936e001ae1bb7
Skwal98 <Audi!*5382>
2023-04-07 23:29:08 +0300

    first impl

---
+++ src/Schedule.TBot/Answers/MainMenuAnswerHandler.cs
@@ -0,0 +1,12 @@
+﻿using Schedule.TBot.Framework;
+
+namespace Schedule.TBot.Answers
+{
+    public class MainMenuAnswerHandler : AnswerHandler
+    {
+        public override Task HandleAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- src/Schedule.TBot/Program.cs
+++ src/Schedule.TBot/Program.cs
@@ -1,5 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Schedule.TBot.Answers;
 
 var serviceProvider = new ServiceCollection();
+
+//await StartAsync(x =>
+//{
+//    x.Default<MainMenuAnswerHandler>();
+//});
