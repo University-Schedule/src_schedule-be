546dc899e56ef10a84fa8f3430013ccb02d6031e
Skwal98 <Audi!*5382>
2023-04-07 23:40:45 +0300

    DependencyInjection

--- src/Schedule.TBot/Program.cs
+++ src/Schedule.TBot/Program.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Schedule.TBot.Answers;
 
-var serviceProvider = new ServiceCollection();
+var serviceCollection = new ServiceCollection();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
 
 //await StartAsync(x =>
 //{
 //    x.Default<MainMenuAnswerHandler>();
 //});
+
+
+var scope = serviceProvider.CreateScope();
+//scope.ServiceProvider.GetService<...>
+scope.Dispose();
